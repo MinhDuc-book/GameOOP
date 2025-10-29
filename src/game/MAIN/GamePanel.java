50b7e613a980c24bf4d456bce5567d8da964c07a
htdat514 <hoangdatvn037@gmail.com>
2025-10-28 23:25:11 -0800

    add level

--- src/game/MAIN/GamePanel.java
+++ src/game/MAIN/GamePanel.java
@@ -21,8 +21,9 @@ import game.HIGHSCORE.HighscoreManager;
 public class GamePanel extends JPanel implements Runnable {
     private JFrame window;
 
-    public GamePanel(JFrame window) {
+    public GamePanel(JFrame window, int level) {
         this.window = window;
+        this.level = level;
         init();
     }
 
@@ -43,6 +44,7 @@ public class GamePanel extends JPanel implements Runnable {
 
     int FPS = 60;
     public int score = 0;
+    public int level = 1; // mặc định là level 1
 
     public static int getSreenWidth() {
         return SCREEN_WIDTH;
@@ -83,6 +85,9 @@ public class GamePanel extends JPanel implements Runnable {
         balls.add(initBall);
 
         aSetter.setObject();
+
+        String mapName = "map" + level;
+        brick.setBrickMap(BrickMapLoader.loadMap(mapName));
 
         gameState.setCurrentState(GameState.State.PLAY);
     }
@@ -263,6 +268,11 @@ public class GamePanel extends JPanel implements Runnable {
                 System.out.println("Boom!");
                 break;
         }
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+        System.out.println("Đã chọn cấp độ: " + level);
     }
 
     @Override
